407d9417635c1c6e10b15f9152bce0ef53359f80
metalx1000 <github@filmsbykris.com>
2017-06-12 10:07:20 -0400

    resize canvas on window resize

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,15 @@
+//resize canvas on window resize
+window.addEventListener( 'resize', onWindowResize, false );
+
+function onWindowResize( event ) {
+  SCREEN_HEIGHT = window.innerHeight;
+  SCREEN_WIDTH  = window.innerWidth;
+  renderer.setSize( SCREEN_WIDTH, SCREEN_HEIGHT );
+  camera.aspect = SCREEN_WIDTH / SCREEN_HEIGHT;
+  camera.updateProjectionMatrix();
+  //composer.reset();
+}
+
 function createCube(d){
   //possition
   if(typeof d.x === "undefined"){d.x = 0;}
